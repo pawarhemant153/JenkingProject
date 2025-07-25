b6769e94fb030b9b15f0887e8ff5295a5125c96a
pawarhemant153 <pawarh639@gmail.com>
2025-07-25 08:35:41 +0530

    Add project files.

---
+++ JenkingProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkingProject
+{
+    internal class Program
+    {
+        void Addition()
+        {
+            int a=10, b=20, c;
+            c = a + b;
+            Console.WriteLine("Addition= " + c);
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Addition();
+            Console.ReadLine();
+        }
+    }
+}
